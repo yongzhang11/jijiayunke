39c7e7021040c6761670155448ee73e9ead49fe8
yongzhang <ayongzhang@outlook.com>
2024-08-05 17:59:21 +0800

    添加用例

--- testcase/test_course.py
+++ testcase/test_course.py
@@ -1,5 +1,7 @@
 # @Time：2024/8/5 13:53
 # @Author: Allan
+import time
+
 from config.supplier.PublicMethod import PublicMethods
 from config.BrowserDriver.drissionpage_driver import DrissionpageDriverConfig
 import pytest
@@ -20,8 +22,32 @@ def some_data():
 @allure.feature("内容管理")
 class TestContent:
     @pytest.mark.content1
-    @allure.title("判断学习课程是否上架")
-    @allure.story("加入班级")
+    @allure.story("导入学习课程")
     def test_create_learning_course(self):
-        text = page.ele('@class=text-danger').text
-        assert text == '未上架', f"Unexpected page title: {text}"
+        try:
+            time.sleep(5)
+            page.refresh()
+            page.wait.ele_displayed('@class=text-danger]', timeout=10)
+            text = page.ele('@class=text-danger').text
+            assert text == '未上架', f"Unexpected page title: {text}"
+        except Exception as e:
+            print(f"测试过程中发生错误: {e}")
+
+    @pytest.mark.content1
+    @allure.story("上架课程")
+    def test_resource_display_correct(self):
+        try:
+            lists = len(page.eles('xpath=//tbody/tr'))
+            name = []
+            for i in range(1, lists):
+                class_names = page.ele(
+                    'xpath://tbody/tr[{}]//h3/a'.format(i)).text
+                name.append(class_names.title())
+            for i in name:
+                if i == '内部测试学习课程0724':
+                    page.ele('@text()={}'.format("内部测试学习课程0724"), timeout=3).click()
+                    break
+            texts = page.ele('@class=number').text
+        except Exception as e:
+            print(f"测试过程中发生错误: {e}")
+        assert texts == '2', f"Unexpected page title: {texts}"
