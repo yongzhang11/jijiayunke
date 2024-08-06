3c878743a7a1b9ca2c582e63437e76a01b9dd3b6
yongzhang <ayongzhang@outlook.com>
2024-08-06 15:36:44 +0800

    运行优化

--- testcase/run.py
+++ testcase/run.py
@@ -1,15 +1,23 @@
-# @Time：2024/8/3 10:45
-# @Author: Allan
 import pytest
 import os
 import datetime
 
-os.chdir('../testcase')
+# 获取当前日期
+current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-current_date = datetime.datetime.now().strftime('%Y-%m-%d')
-if __name__ == '__main__':
-    # html报告
-    # pytest.main(['-m create', '--html=../report/report.html'])
-    # allure报告
-    pytest.main(['-m', 'create or content1', '-s', '-q', '--clean-alluredir', '--alluredir=allure-results'])
-    os.system(r"allure generate -c -o ../report/{}".format(current_date))
+
+def run_tests():
+    pytest.main([
+        '-m', 'content1 or create  ',  # 运行标记为create或content1的测试
+        '-s',  # 显示标准输出
+        '-q',  # 减少输出信息
+        '-v',  # 显示详细信息
+        '-n', '1',  # 使用2个进程并行运行
+        '--clean-alluredir',  # 清除以前的结果
+        '--alluredir=allure-results'  # 指定Allure结果目录
+    ])
+    os.system(r"allure generate -c -o ../report/{}".format(current_date))  # 生成Allure报告
+
+
+if __name__ == "__main__":
+    run_tests()
