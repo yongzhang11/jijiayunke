38bb363509811d3aaf44a2078ba9f30b85c53e81
yongzhang11 <49093210+yongzhang11@users.noreply.github.com>
2024-09-19 10:07:37 +0800

    Create positioned_element.py

---
+++ testcase/course_test/positioned_element.py
@@ -0,0 +1,13 @@
+class element:
+    # #为ID定位
+    确认按钮 = "#ajax-change-confirm-publish-ok"
+    上架按钮 = '#postCourse'
+    整体概况 = '@class=title'
+    保存按钮 = '@class=btn btn-primary'
+    选择证书 = 'xpath://*[@id="certmodel-certid"]/option[2]'
+    证书下来框 = '#certmodel-certid'
+    证书未设置按钮 = 'xpath:/html/body/div[1]/div/div/div[2]/div/div[2]/div/div[2]/div[2]/div/div[5]/div[2]/div/div/a'
+    选择试卷确认按钮 = '@class=btn btn-primary relation-course'
+    选择第一个试卷 = 'xpath://tbody/tr[1]/td[1]/label/span'
+    添加试卷按钮 = '@class=btn btn-primary addForm'
+    试考试卷tab栏 = 'xpath://*[@id="myTab"]/li[2]/a'
